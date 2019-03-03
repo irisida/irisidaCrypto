b86aa86eafb34109649014718c77aac733df8be8
irisida <20445648+irisida@users.noreply.github.com>
2019-03-03 00:56:04 +0000

    added the nonce and difficulty

--- config/config.js
+++ config/config.js
@@ -1,10 +1,14 @@
+const INITIAL_DIFFICULTY = 3
+
 const GENESIS_DATA = {
   timestamp: 1,
-  lastHash: "-----------",
-  hash: "genisishash",
-  data: []
-};
+  lastHash: '-----------',
+  hash: 'genisishash',
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
+  data: [],
+}
 
 module.exports = {
-  GENESIS_DATA
-};
+  GENESIS_DATA,
+}
